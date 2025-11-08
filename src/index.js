76034f9fc70a82ddb9f14b990e3fc31fea5ba2b4
Karan Jamwal <karanjamwal2004@gmail.com>
2025-11-08 18:48:29 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -15,16 +16,6 @@ connectDB()
     console.log("ERROR: ", err);
   });
 
-
-
-
-
-
-
-
-
-
-  
 /*
 const app = express();
 
