631fdd65f03aeef743d785aa1a2572814eb78c37
Karan Jamwal <karanjamwal2004@gmail.com>
2025-11-11 19:51:52 +0530

    small bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -140,7 +140,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
+    secure: false,
   };
 
   return res
@@ -166,7 +166,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   // clear cookies
   // return response
 
-  User.findByIdAndUpdate(
+  await User.findByIdAndUpdate(
     req.user._id,
     {
       $set: {
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,8 +6,8 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
-      req.cookies.accessToken ||
-      req.headers("Authorization")?.replace("Bearer ", "");
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
@@ -15,7 +15,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    const user = await User.findById(decodedToken._id).select(
+    const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
 
