4fdd02d3ab9d4b58d5451a5ca692d571982ebc78
Karan Jamwal <karanjamwal2004@gmail.com>
2025-11-16 18:08:59 +0530

    refactor user routes to include additional secured endpoints for account management and user profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,14 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
-  refreshAccessToken,
   registerUser,
+  getCurrentUser,
+  getWatchHistory,
+  updateUserAvatar,
+  refreshAccessToken,
+  updateAccountDetails,
+  getUserChannelProfile,
+  changeCurrentPassword,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,7 +31,20 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
+
+// secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateAccountDetails);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
